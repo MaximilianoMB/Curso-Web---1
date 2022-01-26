35edb4c8766fb2c9ccaa8a6ebcdff96490f25193
Maximiliano MB <maques.b@gmail.com>
2022-01-26 16:43:38 -0300

    segunda parte

--- JS/calculadora.js
+++ JS/calculadora.js
@@ -3,4 +3,23 @@
 var primeiro = prompt("digite o primeiro numero");
 var segundo = prompt("digite o segundo numero");
 var soma = Number(primeiro) + Number(segundo);
-alert(sum)
+alert(sum);
+
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.  
+
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
+var primeiro = prompt("digite o primeiro numero");
+var segundo = prompt("digite o segundo numero");
+var soma = Number(primeiro) + Number(segundo);
+var sub = Number(primeiro) - Number(segundo);
+var mult = Number(primeiro) * Number(segundo);
+var div = Number(primeiro) / Number(segundo);
+alert("A soma é: " + soma);
+alert("A subtração é: " + sub);
+alert("A multiplicação é: " + mult);
+alert("A divisão é: " + div);
