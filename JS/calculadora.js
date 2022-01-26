f3b1f959aa0bf7ce9b39116a873ddee24c214dd8
Maximiliano MB <maques.b@gmail.com>
2022-01-26 16:12:55 -0300

    segunda parte

---
+++ JS/calculadora.js
@@ -0,0 +1,6 @@
+// PARA RODAR NO CONSOLE DO BROWSER
+
+var primeiro = prompt("digite o primeiro numero");
+var segundo = prompt("digite o segundo numero");
+var soma = Number(primeiro) + Number(segundo);
+alert(sum)
