1d6cf9e3a2209eb0fd300fc37e478dbad1707746
Maximiliano MB <maques.b@gmail.com>
2022-02-01 16:19:35 -0300

    funcoes

--- JS/javaScriptInicio.js
+++ JS/javaScriptInicio.js
@@ -3,3 +3,39 @@ function sayHello() {
 }
 
 sayHello();
+
+var falarTchau = function() {
+    console.log("Tchau!");
+}
+
+falarTchau();
+
+function cantar() {
+    console.log("CACACACACA");
+    console.log("BlaBlaLalala");
+}
+
+cantar();
+
+function letras(letra) {
+    console.log(letra);
+}
+
+letras("bilu bilu teteia");
+letras("aluei istnouei");
+letras("eu ia li chama");
+letras("tchucuracktchucurei");
+
+UTEIS
+
+function multiplicar(a, b) {
+    console.log(a*b);
+}
+
+multiplicar(5, 10);
+
+function dividir(a, b) {
+   return a+b;   
+}
+
+dividir(3, 5);
