892ec94e0214e58921e4a96026c2108b4779fe15
Maximiliano MB <maques.mb@gmail.com>
2022-02-15 10:45:09 -0300

    dom events

--- JS/dom.js
+++ JS/dom.js
@@ -4,3 +4,43 @@ function botao(){
     h1.className = "tituloFoda";
 }
 
+var button = document.getElementById("foda");
+    
+button.addEventListener("click", function() {
+    console.log("Clicou!!");
+})
+
+var entrada = document.getElementById("entrada");
+var input = document.getElementById("input");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function adicionaItem() {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    var ul = document.querySelector("ul");
+    ul.appendChild(li);
+    input.value = '';
+}
+
+function insereBotao() {
+    if (inputLength() > 0 ){
+        adicionaItem();
+    }
+}
+
+function insereEnter() {
+    if (input.value.length > 0 && event.which === 13 ){
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        var ul = document.querySelector("ul");
+        ul.appendChild(li);
+        input.value = '';
+    }
+}
+
+entrada.addEventListener("click", insereBotao); 
+
+input.addEventListener("keypress", insereEnter);
